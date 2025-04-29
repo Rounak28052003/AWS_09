88cd333f1ba98c52ea6b1ebb1d653569e2507490
Rounak28052003 <singhrounak647@gmail.com>
2025-04-29 17:39:51 +0530

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello MCKVIE I am Rounak FROM DS')
+  res.send('Hello MCKVIE I am Rounak SFROM DS')
 })
 
 app.get('/courses', function (req, res) {
